f45ad3d6608535ea2d3d3327d17542efd88cf149
davenkin <davenkin@126.com>
2019-09-05 13:26:10 +0800

    refactor domain event

--- src/main/java/com/ecommerce/shared/event/DomainEvent.java
+++ src/main/java/com/ecommerce/shared/event/DomainEvent.java
@@ -10,19 +10,8 @@ import static java.time.Instant.now;
 
 @Getter
 public abstract class DomainEvent {
-    private String id;
-    private Instant createdAt;
-    private String aggregate;
-
-    @Deprecated
-    protected DomainEvent() {
-    }
-
-    protected DomainEvent(String aggregate) {
-        this.id = UuidGenerator.newUuid();
-        this.createdAt = now();
-        this.aggregate = aggregate;
-    }
+    private String id = UuidGenerator.newUuid();
+    private Instant createdAt = now();
 
     @Override
     public String toString() {
